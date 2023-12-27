fac2e8d05d5165d5c206c96eee36dce3b02f1dbf
raveenGitHub8464 <praveenalladi8464@Gmail.com>
2023-12-27 13:29:38 +0530

    first commit

---
+++ onlineBookStore (2) (1)/onlineBookStore/src/app/book.ts
@@ -0,0 +1,8 @@
+export class Book {
+     bookId: number ;
+     bookName: string ; // Initialize with a default value
+     bookAuthor: string  ; // Initialize with a default value
+     bookPrice: number; // Initialize with a default value
+
+  }
+
